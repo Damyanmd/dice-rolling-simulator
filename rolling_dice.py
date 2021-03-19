ea37bacd570fd61edfe57cf66aefee75deab8bf2
Damyan Doykov <77811406+Damyanmd@users.noreply.github.com>
2021-03-19 12:30:55 +0200

    Add files via upload

---
+++ rolling_dice.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import random
+
+#creating the window
+root = Tk()
+root.geometry('400x400')
+root.title('Dice Rolling Simulator')
+root.resizable(0, 0)
+
+#function for the button
+def rolling_dice():
+    image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    shown_image.configure(image=image1)
+    shown_image.image = image1
+
+#choosing and importing images
+dice = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png']
+random_image = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+shown_image = Label(root, image=random_image)
+shown_image.image = random_image
+shown_image.pack(expand=True)
+
+button = Button(root, text='Roll the Dice',  font='arial 15 bold', bg='white smoke', fg='red2', command=rolling_dice).pack(expand=True)
+
+root.mainloop()
